0d1b08eff18e936f72366f2a446ba3d668c10835
owoc <tomasz.lewoc@gmail.com>
2016-02-22 12:15:40 +0100

    Added files via upload

--- js/gui.js
+++ js/gui.js
@@ -87,10 +87,8 @@ var resetBuild = function() {
 };
 
 var onInit = function(vocation) {
-	alert("1");
   character = planner.init(vocation);
   character = planner.levelUp(character, vocation, 'to10', 0);
-	alert("2");
   setChar(character);
   initChoosen = true;
   readLevels();
